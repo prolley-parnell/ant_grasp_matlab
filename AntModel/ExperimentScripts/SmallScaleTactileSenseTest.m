1d5259139d84fb17de2b6bef302fa2cd61ae56c3
erolley-parnell <erolleyp@gmail.com>
2022-10-28 14:09:42 +0100

    Experiment: sweep IGEF param
    Collected data from a sweep of values for the different IGEF params, and wrote scripts to parse the data and plot it.

---
+++ AntModel/ExperimentScripts/SmallScaleTactileSenseTest.m
@@ -0,0 +1,80 @@
+%% Set the environment by closing any previous figures and variables
+diary lastDiaryFile
+
+close all;
+clear;
+
+
+
+modelFolder = 'C:\Users\eroll\Documents\MATLAB\Model\ant_grasp_matlab\AntModel\';
+scriptFolder = pwd;
+
+cd(modelFolder)
+addpath('MotionControl','ModelAntBody', 'BehaviourControl', 'Environment',...
+    'urdf', 'Grasp', 'ExperimentOutput', 'ExperimentScripts', 'MainScript', 'ToolClasses');
+
+rng('shuffle');
+
+%% ----------- Experiment Set up
+
+RUNTIME_ARGS = RuntimeArgs();
+
+% Warnings are automatically enabled
+RUNTIME_ARGS.disableWarnings();
+
+% Number of Iterations
+RUNTIME_ARGS.N_TRIALS = 50;
+
+NumberOfPoints = [2:1:40];
+%NumberOfPoints = [10:1:40];
+
+nExperiment = length(NumberOfPoints);
+
+RUNTIME_ARGS.RATE = 0.05;
+RUNTIME_ARGS.PLOT.ENABLE = [0 0];
+
+RUNTIME_ARGS.PRINTOUT.ENABLE = 1;
+RUNTIME_ARGS.RECORD.ENABLE = 0;
+
+
+RUNTIME_ARGS.COLLISION_OBJ.SCALE = 0.16;
+RUNTIME_ARGS.COLLISION_OBJ.FILE_PATH = './Environment/12_sided_tiny_shape.stl';
+RUNTIME_ARGS.BODY_MOTION_ENABLE = 0;
+
+
+RUNTIME_ARGS.ANT_POSE = [0 0 0 0 0.3 -0.45 0.85 0.3 -0.45 0.85]';
+% ------------- Antenna Motion -------------- %
+RUNTIME_ARGS.SEARCH_SPACE.SAMPLE.MODE = "fixed";
+RUNTIME_ARGS.SEARCH_SPACE.SAMPLE.VAR = 0.5;
+RUNTIME_ARGS.SEARCH_SPACE.RANGE = [-1, 1; ...
+    2.5, 3.5; ...
+    0, 1];
+RUNTIME_ARGS.ANTENNA_CONTROL =  ["goals", "joint_traj"];
+
+%No need for a threshold, as it picks the best after 10 contacts, and no
+%new goal is generated
+RUNTIME_ARGS.SENSE.THRESH = 0;
+RUNTIME_ARGS.SENSE.MODE = "force_align";
+
+
+RUNTIME_ARGS.SEARCH_SPACE.REFINE.MODE = 'IG';
+RUNTIME_ARGS.SEARCH_SPACE.REFINE.PARAM = [1.2, 0.5, 1.4, 0.9];
+
+RUNTIME_ARGS_i = repmat(RUNTIME_ARGS, [1, nExperiment]);
+
+for i = 1: nExperiment
+    RUNTIME_ARGS_i(i).TRIAL_NAME = ['TunedIGEFAlignDiceObj2-40\', int2str(NumberOfPoints(i)), '_ContactPts_TunedIGEFAlignDice'];
+    RUNTIME_ARGS_i(i).ANT_MEMORY = NumberOfPoints(i);
+    RUNTIME_ARGS_i(i).SENSE.MINIMUM_N = NumberOfPoints(i);
+end
+
+tic
+p = gcp;
+parfevalOnAll(p,@warning, 0,'off');
+opts = parforOptions(p);
+parfor (n = 1:nExperiment, opts)
+    [exitflag, fileHandler] = AntModelFunction(RUNTIME_ARGS_i(n));
+end
+toc
+
+diary off
